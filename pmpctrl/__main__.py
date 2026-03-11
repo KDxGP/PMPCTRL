73ff7254e08d9d776bf5c5a375c498e263533aaf
KDxGP <kdxgp@proton.me>
2026-03-11 22:59:34 +0100

    prepared for config parser checks

--- pmpctrl/__main__.py
+++ pmpctrl/__main__.py
@@ -19,6 +19,8 @@ from pmpctrl.valve_control import ValveControl
 from threading import Thread
 from time import sleep
 
+__version__ = "1.0.0-dev"
+
 class Settings:
     # logging
     LOG_LEVEL = logging.WARNING
@@ -26,6 +28,11 @@ class Settings:
     API_PORT = 8000
     # pressure_control
     PRESSURE_CONTROL_CYCLE_TIME = 0.01
+    PRESSURE_CONTROL_TOLERANCE_PLUS = 0.0
+    PRESSURE_CONTROL_TOLERANCE_MINUS = 10.0
+    PRESSURE_CONTROL_SETPOINT = 962.9274
+    PRESSURE_CONTROL_PRESSURE_MAX = 1084.0
+    PRESSURE_CONTROL_PRESSURE_MIN = 450.0
     # pressure_sensor
     PRESSURE_SENSOR_CYCLE_TIME = 0.01
     PRESSURE_SENSOR_BUS_NR = 1
@@ -36,6 +43,14 @@ class Settings:
     # valve_coontrol
     VALVE_CONTROL_CYCLE_TIME = 0.1
     VALVE_CONTROL_PIN_NUMBER = 23
+    # mode_hold
+    MODE_HOLD_PRESSURE_TARGET = 875.0
+    # mode_interval
+    MODE_INTERVAL_PEAK_PRESSURE = 790.0
+    MODE_INTERVAL_INTERVAL_TIME = 20.0
+    # mode_pulsating
+    MODE_PULSATING_PUMP_TIME = 2.7
+    MODE_PULSATING_RELEASE_TIME = 1.7
 
 
 def parse_arguments():
@@ -44,56 +59,137 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def parse_config(control_data: ControlData, config_file: str) -> Settings:
+def parse_config(config_file: str) -> Settings:
+    logger = logging.getLogger(__name__)
+    settings = Settings
     config = configparser.ConfigParser()
     config.read(config_file)
-    settings = Settings
 
-    # logging
-    config_log_level = config.get('logging', 'log_level')
-    logging_log_levels = logging.getLevelNamesMapping().items()
-    for key, value in logging_log_levels:
-        if config_log_level in key:
-            settings.LOG_LEVEL = value
+    try:
+        # parse all parameters
+        ini_logging_log_level = config.get('logging', 'log_level')
+        ini_api_port = config.getint('api', 'port')
+        ini_pressure_control_cycle_time = config.getfloat('pressure_control', 'cycle_time')
+        ini_pressure_control_tolerance_plus = config.getfloat('pressure_control', 'tolerance_plus')
+        ini_pressure_control_tolerance_minus = config.getfloat('pressure_control', 'tolerance_minus')
+        ini_pressure_control_setpoint = config.getfloat('pressure_control', 'setpoint')
+        ini_pressure_control_pressure_max = config.getfloat('pressure_control', 'pressure_max')
+        ini_pressure_control_pressure_min = config.getfloat('pressure_control', 'pressure_min')
+        ini_pressure_sensor_cycle_time = config.getfloat('pressure_sensor', 'cycle_time')
+        ini_pressure_sensor_bus_nr = config.getint('pressure_sensor', 'smbus_nr')
+        ini_pressure_sensor_i2c_adr = int(config.get('pressure_sensor', 'i2c_address'), 0)
+        ini_pump_control_cycle_time = config.getfloat('pump_control', 'cycle_time')
+        ini_pump_control_pin_number = config.getint('pump_control', 'pin_number')
+        ini_valve_control_cycle_time = config.getfloat('valve_control', 'cycle_time')
+        ini_valve_control_pin_number = config.getint('valve_control', 'pin_number')
+        ini_mode_hold_pressure_target = config.getfloat('mode_hold', 'pressure_target')
+        ini_mode_interval_peak_pressure = config.getfloat('mode_interval', 'peak_pressure')
+        ini_mode_interval_interval_time = config.getfloat('mode_interval', 'interval_time')
+        ini_mode_pulsating_pump_time = config.getfloat('mode_pulsating', 'pump_time')
+        ini_mode_pulsating_release_time = config.getfloat('mode_pulsating', 'release_time')
+    
 
-    control_data.set_log_level(settings.LOG_LEVEL)
+        # [logging]
+        # check if in loglevels
+        logging_log_levels = logging.getLevelNamesMapping().items()
+        logging_log_level_false = True
+        for key, value in logging_log_levels:
+            if ini_logging_log_level in key:
+                settings.log_level = value
+                logging_log_level_false = False
+        if logging_log_level_false:
+            logger.error(f"config.ini: {ini_logging_log_level} is not a valid log level, using default value {settings.LOG_LEVEL}")
+        # [api]
+        # check if port is in range
+        if 1024 < ini_api_port <= 65535:
+            settings.API_PORT = ini_api_port
+        else:
+            logger.error(f"config.ini: API port not in range (1025-65535), using default value {settings.API_PORT}")
 
-    # api
-    settings.API_PORT = config.getint('api', 'port')
+        # [pressure_control]
+        # cycle_time
+        # TODO
+        settings.PRESSURE_CONTROL_CYCLE_TIME = ini_pressure_control_cycle_time
 
-    # pressure_control
-    settings.PRESSURE_CONTROL_CYCLE_TIME = config.getfloat('pressure_control', 'cycle_time')
-    control_data.set_pressure_target_tolerance_plus(config.getfloat('pressure_control', 'tolerance_plus'))
-    control_data.set_pressure_target_tolerance_minus(config.getfloat('pressure_control', 'tolerance_minus'))
-    control_data.set_pressure_setpoint(config.getfloat('pressure_control', 'setpoint'))
-    control_data.set_pressure_max(config.getfloat('pressure_control', 'pressure_max'))
-    control_data.set_pressure_min(config.getfloat('pressure_control', 'pressure_min'))
+        # tolerance_plus
+        # check if in range
+        if 0.0 <= ini_pressure_control_tolerance_plus <= 50.0:
+            settings.PRESSURE_CONTROL_TOLERANCE_PLUS = ini_pressure_control_tolerance_plus
+        else:
+            logger.error(f"config.ini: pressure target tolerance PLUS+ not in range (0-50), using default value {settings.PRESSURE_CONTROL_TOLERANCE_PLUS}")
+            
+        # tolerance_minus
+        # check if in range
+        if 0.0 <= ini_pressure_control_tolerance_minus <= 50.0:
+            settings.PRESSURE_CONTROL_TOLERANCE_MINUS = ini_pressure_control_tolerance_minus
+        else:
+            logger.error(f"config.ini: pressure target tolerance MINUS- not in range (0-50), using default value {settings.PRESSURE_CONTROL_TOLERANCE_MINUS}")
+            
+        # setpoint
+        # TODO
+        # check if in range, but what range? min/max?
+        settings.PRESSURE_CONTROL_SETPOINT = ini_pressure_control_setpoint
+        
+        # [pressure_sensor]
+        # TODO
+        settings.PRESSURE_SENSOR_CYCLE_TIME = ini_pressure_sensor_cycle_time
+        settings.PRESSURE_SENSOR_BUS_NR = ini_pressure_sensor_bus_nr
+        settings.PRESSURE_SENSOR_I2C_ADR = ini_pressure_sensor_i2c_adr
 
-    # pressure_sensor
-    settings.PRESSURE_SENSOR_CYCLE_TIME = config.getfloat('pressure_sensor', 'cycle_time')
-    settings.PRESSURE_SENSOR_BUS_NR = config.getint('pressure_sensor', 'smbus_nr')
-    settings.PRESSURE_SENSOR_I2C_ADR = int(config.get('pressure_sensor', 'i2c_address'), 0)
+        # [pump_control]
+        # TODO
+        settings.PUMP_CONTROL_CYCLE_TIME = ini_pump_control_cycle_time
+        settings.PUMP_CONTROL_PIN_NUMBER = ini_pump_control_pin_number
 
-    # pump_control
-    settings.PUMP_CONTROL_CYCLE_TIME = config.getfloat('pump_control', 'cycle_time')
-    settings.PUMP_CONTROL_PIN_NUMBER = config.getint('pump_control', 'pin_number')
+        # [valve_control]
+        # TODO
+        settings.VALVE_CONTROL_CYCLE_TIME = ini_valve_control_cycle_time
+        settings.VALVE_CONTROL_PIN_NUMBER = ini_valve_control_pin_number
 
-    # valve_control
-    settings.VALVE_CONTROL_CYCLE_TIME = config.getfloat('valve_control', 'cycle_time')
-    settings.VALVE_CONTROL_PIN_NUMBER = config.getint('valve_control', 'pin_number')
+        # [mode_hold]
+        # TODO
+        # target between min/max
+        settings.MODE_HOLD_PRESSURE_TARGET = ini_mode_hold_pressure_target
+        
+        # [mode_interval]
+        # peak_pressure
+        # TODO
+        # in range min/max
+        settings.MODE_INTERVAL_PEAK_PRESSURE = ini_mode_interval_peak_pressure
+        # interval_time
+        # >0s?
+        settings.MODE_INTERVAL_INTERVAL_TIME = ini_mode_interval_interval_time
 
-    # mode_hold
-    control_data.set_pressure_target(config.getfloat('mode_hold', 'pressure_target'))
-
-    # mode_interval
-    control_data.set_mode_interval_peak_pressure(config.getfloat('mode_interval', 'peak_pressure'))
-    control_data.set_mode_interval_time(config.getfloat('mode_interval', 'interval_time'))
-
-    # mode_pulsating
-    control_data.set_mode_pulsating_pump_time(config.getfloat('mode_pulsating', 'pump_time'))
-    control_data.set_mode_pulsating_release_time(config.getfloat('mode_pulsating', 'release_time'))
+        # [mode_pulsating]
+        # pump_time
+        # >0s?
+        settings.MODE_PULSATING_PUMP_TIME = ini_mode_pulsating_pump_time
+        # release_time
+        # >0s? but smaller than overheating valve[]
+        settings.MODE_PULSATING_RELEASE_TIME = ini_mode_pulsating_release_time
+    except Exception as e:
+        logger.error(f"config.ini: non-valid values, using defaults")
+        logger.error(f"config.ini: {e}")
 
     return settings
+
+def apply_settings(control_data: ControlData, settings: Settings):
+    # [logging]
+    control_data.set_log_level(settings.LOG_LEVEL)
+    # [pressure_control]
+    control_data.set_pressure_target_tolerance_plus(settings.PRESSURE_CONTROL_TOLERANCE_PLUS)
+    control_data.set_pressure_target_tolerance_plus(settings.PRESSURE_CONTROL_TOLERANCE_MINUS)
+    control_data.set_pressure_setpoint(settings.PRESSURE_CONTROL_SETPOINT)
+    control_data.set_pressure_max(settings.PRESSURE_CONTROL_PRESSURE_MAX)
+    control_data.set_pressure_min(settings.PRESSURE_CONTROL_PRESSURE_MIN)
+    # [mode_hold]
+    control_data.set_pressure_target(settings.MODE_HOLD_PRESSURE_TARGET)
+    # [mode_interval]
+    control_data.set_mode_interval_peak_pressure(settings.MODE_INTERVAL_PEAK_PRESSURE)
+    control_data.set_mode_interval_time(settings.MODE_INTERVAL_INTERVAL_TIME)
+    # [mode_pulsating]
+    control_data.set_mode_pulsating_pump_time(settings.MODE_PULSATING_PUMP_TIME)
+    control_data.set_mode_pulsating_release_time(settings.MODE_PULSATING_RELEASE_TIME)
 
 
 def init_pressure_sensore(control_data: ControlData, settings: Settings) -> Thread:
@@ -203,10 +299,11 @@ def run(control_data: ControlData, settings: Settings):
 def main():
     args = parse_arguments()
     control_data = ControlData()
+    settings = parse_config(args.config)
+    apply_settings(control_data, settings)
     control_data.event_run.set()
     sigterm_handler = partial(shutdown, control_data=control_data)
     signal.signal(signal.SIGTERM, sigterm_handler)
-    settings = parse_config(control_data, args.config)
     run(control_data, settings)
     if not control_data.event_error.is_set():
         sys.exit(0)
